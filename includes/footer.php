4b492ab8ac4056e53aba41d774457fe137c24bdf
System Administrator <root@Hugos-iMac.local>
2016-11-08 10:49:16 -0500

    updated files

--- includes/footer.php
+++ includes/footer.php
@@ -48,7 +48,7 @@
 
 
     <!-- Le Javascript -->
-    <!--<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>-->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="_/js/jquery-3.1.1.min.js"><\/script>');</script>
     <script src="_/js/materialize.js"></script>
     <script src="_/js/scripts.js"></script>
