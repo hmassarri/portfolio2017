4925dd90e9d0e407e599d5d40a5b6707387f2d03
Hugo Massarri <hmassarri@Hugos-iMac.local>
2016-11-18 16:45:00 -0500

    added images and layout for portfolio-web page

--- blog.php
+++ blog.php
@@ -1,131 +1,118 @@
 <?php require_once 'includes/header.php'; ?>
-
-<body id="Blog">
-   
-    <?php require_once 'includes/menu.php'; ?>
-
-    <main>
-        
-           
-        <div id="b-title" class="row white-text">
-            <div class="col s12 ">
+    <body id="Blog">
+        <?php require_once 'includes/menu.php'; ?>
+            <main>
+                <div id="b-title" class="row white-text">
+                    <div class="col s12 ">
+                        <div class="container">
+                            <h1>The Blog.</h1>
+                        </div>
+                    </div>
+                </div>
+                <!--<div id="breadcrumbs" class="container">
+                    <div class="row">
+                        <div class="col s12">
+                            <nav class="white">
+                                <div class="nav-wrapper">
+                                    <div class="col s12">
+                                        <a href="#!" class="breadcrumb black-text">Link 1</a>
+                                        <a href="#!" class="breadcrumb black-text">Link 1</a>
+                                        <a href="#!" class="breadcrumb black-text">Link 1</a>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>-->
+                
+                <!-- Start columns -->
                 <div class="container">
-                    <h1>The Blog.</h1>            
-                </div>
-            </div>
-        </div>
-        
-        <!--<div id="breadcrumbs" class="container">
-            <div class="row">
-                <div class="col s12">
-                    <nav class="white">
-                        <div class="nav-wrapper">
-                            <div class="col s12">
-                                <a href="#!" class="breadcrumb black-text">Link 1</a>
-                                <a href="#!" class="breadcrumb black-text">Link 1</a>
-                                <a href="#!" class="breadcrumb black-text">Link 1</a>
+                    <div class="row">
+
+                        <!-- Left Column -->
+                        <div class="col s12 m12 l5">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/ledsledcustoms.jpg">
+                                    <span class="card-title">Led Sled Customs Panhead Covers</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>Found these awesome Panhead covers on Led Sled Customs. They fit all 1986 - 2106 Sportsters. They come in raw, powdercoat, or chrome. Definitely interested in getting a pair of these. Go check it out.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.ledsledcustoms.com">www.ledsledcustoms.com</a>
+                                </div>
                             </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>-->
-        
-        <div class="container">
-                    
-            <div class="row">
-               
-                <!-- Left Column -->
-                <div class="col s12 m12 l5">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/ledsledcustoms.jpg">
-                            <span class="card-title">Led Sled Customs Panhead Covers</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Found these awesome Panhead covers on Led Sled Customs.  They fit all 1986 - 2106 Sportsters.  They come in raw, powdercoat, or chrome.  Definitely interested in getting a pair of these.  Go check it out.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.ledsledcustoms.com">www.ledsledcustoms.com</a>
-                        </div>
-                    </div>
-                    
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/gringo-s.jpg">
-                            <span class="card-title">Biltwell Gringo S Fullface</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Just picked up this helmet.  I used a SHOEI X11 for the past 8 years and it was an excellent helmet.  Now that I switched up bikes, I decided to try something different.  This is one awesome helmet for the price.  I was very happy with the fit, the comfort and the look and the weight.  Definitely recommend one of these if you are in the market for a new helmet.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.biltwellinc.com">www.biltwellinc.com</a>
-                        </div>
-                    </div>
-                    
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/autodromo-group-b-watch-aBlogtoWatch-24.jpg">
-                            <span class="card-title">Autodromo Group B</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Beautiful.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.autodromo.com">www.autodromo.com</a>
-                        </div>
-                    </div>
-                    
-                </div>
 
-               
-                <!-- Right Column -->
-                <div class="col s12 m12 l7">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/speedway-tuckroll.jpg">
-                            <span class="card-title">Biltwell Speedway Seat</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Thinking about buying this seat in the near future.  If you’re a lone wolf on rubber-mount Sporty, consider ditching your stock two-up seat and replacing it with a Biltwell Speedway unit. Same lean silhouette and quality construction as our HB seat, but chopped to provide comfortable seating for one.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.biltwellinc.com">www.biltwellinc.com</a>
-                        </div>
-                    </div>
-                    
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/Screen%20Shot%202016-11-12%20at%209.46.14%20PM.png">
-                            <span class="card-title">NB 990</span>
-                        </div>
-                        <div class="card-content">
-                            <p>New Balance 990s in Black / Grey.  Awesome.  Get yours.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.newbalance.com">www.newbalance.com</a>
-                        </div>
-                    </div>
-                    
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" src="img/blog/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg">
-                            <span class="card-title">Card Title</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Holy Cow.  One of the best shows I have seen this year.  Amazing.  Definitely recommend.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="http://www.netflix.com/strangerthings">www.netflix.com/strangerthings</a>
-                        </div>
-                    </div>
-                    
-                </div>
-                
-                
-            </div>
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/gringo-s.jpg">
+                                    <span class="card-title">Biltwell Gringo S Fullface</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>Just picked up this helmet. I used a SHOEI X11 for the past 8 years and it was an excellent helmet. Now that I switched up bikes, I decided to try something different. This is one awesome helmet for the price. I was very happy with the fit, the comfort and the look and the weight. Definitely recommend one of these if you are in the market for a new helmet.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.biltwellinc.com">www.biltwellinc.com</a>
+                                </div>
+                            </div>
 
-        </div>
-    </main>
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/autodromo-group-b-watch-aBlogtoWatch-24.jpg">
+                                    <span class="card-title">Autodromo Group B</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>Beautiful.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.autodromo.com">www.autodromo.com</a>
+                                </div>
+                            </div>
+                        </div><!-- end left col -->
 
-    <?php require_once 'includes/footer.php'; ?>
+                        <!-- Right Column -->
+                        <div class="col s12 m12 l7">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/speedway-tuckroll.jpg">
+                                    <span class="card-title">Biltwell Speedway Seat</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>Thinking about buying this seat in the near future. If you’re a lone wolf on rubber-mount Sporty, consider ditching your stock two-up seat and replacing it with a Biltwell Speedway unit. Same lean silhouette and quality construction as our HB seat, but chopped to provide comfortable seating for one.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.biltwellinc.com">www.biltwellinc.com</a>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/Screen%20Shot%202016-11-12%20at%209.46.14%20PM.png">
+                                    <span class="card-title">NB 990</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>New Balance 990s in Black / Grey. Awesome. Get yours.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.newbalance.com">www.newbalance.com</a>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <div class="card-image">
+                                    <img class="responsive-img" src="img/blog/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg">
+                                    <span class="card-title">Card Title</span>
+                                </div>
+                                <div class="card-content">
+                                    <p>Holy Cow. One of the best shows I have seen this year. Amazing. Definitely recommend.</p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="http://www.netflix.com/strangerthings">www.netflix.com/strangerthings</a>
+                                </div>
+                            </div>
+                        </div><!-- end right col -->
+                    </div><!-- end row -->
+                </div><!-- end container -->
+            </main>
+            <?php require_once 'includes/footer.php'; ?>
--- homepage.php
+++ homepage.php
@@ -1,10 +1,10 @@
 <?php require_once 'includes/header.php'; ?>
 
-<body id="homepage">
-    <?php require_once 'includes/menu.php'; ?>
-        
-        <main>
-            
-        </main>
+    <body id="homepage">
+        <?php require_once 'includes/menu.php'; ?>
 
-<?php require_once 'includes/footer.php'; ?>
+            <main>
+
+            </main>
+
+            <?php require_once 'includes/footer.php'; ?>
