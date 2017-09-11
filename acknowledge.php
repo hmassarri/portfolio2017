ce2dc3475902f427cab21d271d46cc26c4f33a8d
Hugo M <hugo@hugomassarri.com>
2017-09-11 12:41:33 -0400

    Commit sent

--- acknowledge.php
+++ acknowledge.php
@@ -3,14 +3,14 @@
 
 <?php
     if (isset($_POST['send'])) {
-        $to = 'hugo@hugomassarri.com';
+        $to = 'test@testemail.com';
         $subject = 'Feedback from hugomassarri.com';
         
         $message = 'Name: ' . $_POST['name'] . "\r\n";
         $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
         $message .= 'Comments: ' . $_POST['comments'] . "\r\n";
         
-        $headers = "From: website@hugomassari.com\r\n";
+        $headers = "From: test@testemail.com\r\n";
         $headers .= 'Content-Type: text/plain; charset=utf-8';
         
         $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
@@ -26,16 +26,31 @@
 <div class="container">
     <div class="row">
         <div class="col s12 m12 l12 center-align">
-            <div class="gap-small">&nbsp;</div>
+            
             <?php 
-                if (empty($message)) {
-                    if (isset($success) && $success) { ?>
-                        <h4 class="pink-text text-lighten-3">What did you do?  Me neither.</h4>
-                        <p>No worries. Everything will be ok. Hit back and fill out the form again.</p>
-            <?php } } else { ?>
-                        <h4 class="pink-text text-lighten-3">Awesome! and Thank you.</h4>
-                        <p>Thanks for message.  I'll get back to you soon.</p>
-            <?php } ?>
+                if (empty($_POST['email'])) {
+            ?>
+                        <i class="large material-icons pink-text text-lighten-3">warning</i>
+                        <h4 class="pink-text text-lighten-3">What did you do?? Oh boy ...</h4>
+                        <p class="center-align">It looks likes some of the form fields weren't filled out.</p>
+                        <p>&nbsp;</p>
+                        <p class="center-align">No worries. Everything will be ok.</p>
+                        <p class="center-align">Hit back and fill out the form again.</p>
+                        <i class="pink-text text-lighten-3 medium material-icons">favorite</i>
+            <?php 
+                } 
+                else {
+                    if (!empty($_POST['email'])) {
+                        if (isset($success) && $success) { 
+            ?>      
+                            <h4 class="pink-text text-lighten-3">Awesome! and Thank you.</h4>
+                            <i class="pink-text text-lighten-3 large material-icons">thumb_up</i>
+                            <p>Thanks for message.  I'll get back to you soon.</p>
+            <?php 
+                        } 
+                    }
+                }
+            ?>
             <div class="gap-small">&nbsp;</div>
         </div>
     </div>
