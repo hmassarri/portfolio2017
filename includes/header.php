e1fb00be0e53bf85a05e0cb3cf9841c8633e7870
Hugo Massarri <hmassarri@Hugos-iMac.local>
2017-09-12 10:31:59 -0400

    updated cdn

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
 
 	<title>Hugo M. / UIX Designer and Developer</title>
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 	<link rel="stylesheet" href="_/css/override.css">
 
 	<!--Import Google Icon Font-->
--- includes/javascript.php
+++ includes/javascript.php
@@ -4,7 +4,7 @@
     <script>window.jQuery || document.write('<script src="_/js/jquery-3.1.1.min.js"><\/script>');</script>
     -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript" ></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js" type="text/javascript" ></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js" type="text/javascript" ></script>
 	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js" type="text/javascript"></script>
     <script src="_/js/scripts.js" type="text/javascript" ></script>
 </body>
