36d6b0a7e91af6c708ace820ef2d0b19c7184729
Hugo Massarri <hmassarri@Hugos-iMac.local>
2017-10-05 16:06:39 -0400

    moved to SASS

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 	<title>Hugo M. / UIX Designer and Developer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-	<link rel="stylesheet" href="_/css/override.css">
+	<link rel="stylesheet" href="_/css/base.css">
 
 	<!--Import Google Icon Font-->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
